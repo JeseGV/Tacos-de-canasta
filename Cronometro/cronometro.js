5f28cabfa0d9acb674526308ae6fa79b5ede868f
JeseGV <d.villegas1579@gmail.com>
2022-10-31 11:47:43 -0700

    Co-authored-by: EDGAR ALEJANDRO GUANGORENA ARELLANO <L20550175@chihuahua2.tecnm.mx>

--- Cronometro/cronometro.js
+++ Cronometro/cronometro.js
@@ -1,6 +1,35 @@
 //varibles
-let inicio, final,r;
 
+function stopwatch(){
+    let duration = 0;
+    let startTime =0;
+    let flagTimer =true;
+
+    //Start
+    this.start = function() {
+        if(flagTimer){
+            throw new Error("El cronometro ya ha iniciado...");
+        }
+        startTime = new Date().getTime;
+        flagTimer = true;
+    }
+
+    //stop
+    this.stop =function(){
+        if(!flagTimer){
+            throw new Error("El cronometro no estba iniciado...")   
+        }
+        flagTimer = false;
+        duration = (new Date().getTime() - startTime/1000);
+    }
+
+    //restart
+    this.restart = function(){
+        duration = 0;
+    }
+    //Object.defineProperty(this, 'duration', (get: Function() {return duration}))
+}
+/*
 //Funcion de inico
 function start() {
     if(inicio!==0){
@@ -26,4 +55,4 @@ inicio = 0;
 final = 0;
 r = 0;
 console.log("00:00");
-}
+}*/
